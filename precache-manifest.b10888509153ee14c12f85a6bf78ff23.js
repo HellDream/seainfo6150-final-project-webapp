144028bd72dc535170c67dae1fd5300f6a57e924
Zhenyu Ye <yzy673598755@gmail.com>
2020-11-29 00:49:57 -0800

    Updates

---
+++ precache-manifest.b10888509153ee14c12f85a6bf78ff23.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2042a22a5e7f78b1eca7170c2390eb23",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "5cbaa82ab46d86e39eca",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.ede2b4aa.chunk.css"
+  },
+  {
+    "revision": "eb806664f027c3f2615e",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.e44a74e5.chunk.js"
+  },
+  {
+    "revision": "5cbaa82ab46d86e39eca",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.71aca22a.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "2cd88d8bb952a1290fec581dfdea095f",
+    "url": "/seainfo6150-final-project-webapp/static/media/back.2cd88d8b.svg"
+  },
+  {
+    "revision": "809949fcda59df7c8b321d9f8802d050",
+    "url": "/seainfo6150-final-project-webapp/static/media/books.809949fc.jpg"
+  },
+  {
+    "revision": "1363c2308c5520231481de1c09caf692",
+    "url": "/seainfo6150-final-project-webapp/static/media/close.1363c230.svg"
+  },
+  {
+    "revision": "cd282f889be1bb9a7487e0aeeedb6290",
+    "url": "/seainfo6150-final-project-webapp/static/media/electronics.cd282f88.jpeg"
+  },
+  {
+    "revision": "b91f3a04cf1523fe719ee56c122df67b",
+    "url": "/seainfo6150-final-project-webapp/static/media/entertainments.b91f3a04.png"
+  },
+  {
+    "revision": "f16c276adb2ab93a7fe95ee62f6589b5",
+    "url": "/seainfo6150-final-project-webapp/static/media/furnitures.f16c276a.jpg"
+  },
+  {
+    "revision": "da1df9a46ce66d9b50afdd8bc68ab939",
+    "url": "/seainfo6150-final-project-webapp/static/media/others.da1df9a4.jpg"
+  },
+  {
+    "revision": "fb5015cc738e28456b14006c6196b031",
+    "url": "/seainfo6150-final-project-webapp/static/media/tools.fb5015cc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seainfo6150-final-project-webapp/precache-manifest.e7c8d0c687995ec427bda817642e16d5.js"
+  "/seainfo6150-final-project-webapp/precache-manifest.b10888509153ee14c12f85a6bf78ff23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/seainfo6150-final-project-webapp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
