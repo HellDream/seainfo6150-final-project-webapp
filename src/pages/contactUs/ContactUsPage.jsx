4c40be1861f50c05a313c50f1410141d7eebfe93
Zhenyu Ye <yzy673598755@gmail.com>
2020-12-02 15:16:37 -0800

    fix contact us page and loading progress

--- src/pages/contactUs/ContactUsPage.jsx
+++ src/pages/contactUs/ContactUsPage.jsx
@@ -1,13 +1,43 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './ContactUsPage.module.css';
 import Header from '../../components/headers/Header.jsx';
+import Dialog from '../../components/dialog/Dialog';
+
 const ContactUsPage = (props) => {
+    const [dialogState, setDialogState] = useState({
+        open: false,
+        message: '',
+        ok: null,
+        cancel: null,
+        onClose: null,
+    });
     useEffect(()=>{
         document.title = "Thrift Store - Contact Us";
 
     });
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        setDialogState({
+            open: true,
+            message: 'You have uploaded your item successfully!',
+            ok: () => {
+                setDialogState({ ...dialogState, open: false });
+                props.history.goBack();
+            },
+            onClose: () => {
+                setDialogState({ ...dialogState, open: false });
+            },
+        });  
+    }
     return (
         <>
+            <Dialog
+                open={dialogState.open}
+                message={dialogState.message}
+                ok={dialogState.ok}
+                onClose={dialogState.onClose}
+            />
             <Header
                 createPage={true}
                 onCancel={() => {
@@ -19,7 +49,7 @@ const ContactUsPage = (props) => {
                     <h3 className={styles.formTitle}>
                         Tell Us What You Think!
                     </h3>
-                    <form className={styles.form}>
+                    <form className={styles.form} onSubmit={handleSubmit}>
                         <div className={styles.inputContainer}>
                             <label  className={styles.label} htmlFor="message">Your Email:</label>
                             <input type="email" className={styles.input} required />
