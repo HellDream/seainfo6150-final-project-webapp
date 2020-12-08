22e9bb089bf20bd72a66a4a55e51dc300fcfa50b
Zhenyu Ye <yzy673598755@gmail.com>
2020-12-08 14:56:20 -0800

    modify contact us page

--- src/pages/contactUs/ContactUsPage.jsx
+++ src/pages/contactUs/ContactUsPage.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import styles from './ContactUsPage.module.css';
 import Header from '../../components/headers/Header.jsx';
 import Dialog from '../../components/dialog/Dialog';
+import LoadingProgress from '../../components/progress/LoadingProgress';
 
 const ContactUsPage = (props) => {
     const [dialogState, setDialogState] = useState({
@@ -11,24 +12,52 @@ const ContactUsPage = (props) => {
         cancel: null,
         onClose: null,
     });
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({email:'', message:''});
     useEffect(()=>{
         document.title = "Thrift Store - Contact Us";
-
     });
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = async (event) =>{
         event.preventDefault();
-        setDialogState({
-            open: true,
-            message: 'You have uploaded your item successfully!',
-            ok: () => {
-                setDialogState({ ...dialogState, open: false });
-                props.history.goBack();
+        setLoading(true);
+        const res = await fetch('https://us-central1-seainfo6150-final-project.cloudfunctions.net/api/contactUs',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', 
             },
-            onClose: () => {
-                setDialogState({ ...dialogState, open: false });
-            },
-        });  
+            body: JSON.stringify(data)
+        });
+        setLoading(false);
+        if(res.status!==201){
+            setDialogState({
+                open: true,
+                message: 'Something went wrong with the process...',
+                ok: () => {
+                    setDialogState({ ...dialogState, open: false });
+                },
+                onClose: () => {
+                    setDialogState({ ...dialogState, open: false });
+                },
+            });
+        }else {
+            setDialogState({
+                open: true,
+                message: 'You have uploaded your item successfully!',
+                ok: () => {
+                    setDialogState({ ...dialogState, open: false });
+                    props.history.goBack();
+                },
+                onClose: () => {
+                    setDialogState({ ...dialogState, open: false });
+                },
+            });  
+
+        }
+    }
+
+    const handleChange = (event)=>{
+        setData({...data, [event.target.name]:event.target.value});
     }
     return (
         <>
@@ -44,6 +73,7 @@ const ContactUsPage = (props) => {
                     props.history.goBack();
                 }}
             />
+            {loading  && <LoadingProgress />}
             <section className={styles.section}>
                 <div className={styles.container}>
                     <h3 className={styles.formTitle}>
@@ -52,7 +82,7 @@ const ContactUsPage = (props) => {
                     <form className={styles.form} onSubmit={handleSubmit}>
                         <div className={styles.inputContainer}>
                             <label  className={styles.label} htmlFor="message">Your Email:</label>
-                            <input type="email" className={styles.input} required />
+                            <input name="email" type="email" className={styles.input} required onChange={handleChange} />
                         </div>
                         <div className={styles.inputContainer}>
                             <label className={styles.label} htmlFor="message">
@@ -64,6 +94,7 @@ const ContactUsPage = (props) => {
                                 id="message"
                                 name="message"
                                 required
+                                onChange={handleChange}
                             />
                         </div>
                         <button className={styles.submitButton} type="submit">
