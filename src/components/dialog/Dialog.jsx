d76c18aa33b041f547f25324d8d38f1b0c641c94
Zhenyu Ye <yzy673598755@gmail.com>
2020-12-10 23:09:44 -0800

    dialog accessibility

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -7,17 +7,30 @@ const Dialog = (props) => {
     const onOk = props.ok;
     let dialog = (
         <div className={styles.container}>
-            <div className={styles.dialogWrapper}>
-                <button className={styles.closeBtn} onClick={()=>props.onClose(!open)}> </button>
+            <div
+                role="dialog"
+                aria-modal={true}
+                aria-labelledby="dialog_label"
+                className={styles.dialogWrapper}
+            >
+                <button
+                    className={styles.closeBtn}
+                    onClick={() => props.onClose(!open)}
+                    autoFocus={true}
+                ></button>
                 <h3 className={styles.content}>{message}</h3>
-                {onOk && <button className={styles.okBtn} onClick={onOk}>OK</button>}
+                {onOk && (
+                    <button className={styles.okBtn} onClick={onOk}>
+                        OK
+                    </button>
+                )}
             </div>
         </div>
     );
     if (!open) {
         dialog = null;
     }
-    return open ? dialog:<div></div>;
+    return open ? dialog : <div></div>;
 };
 
 export default Dialog;
